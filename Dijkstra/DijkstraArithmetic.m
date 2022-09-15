d0e5609d0ce5c7c06162a9e5cf0ece528685b4fa
ErnestLiu <44372215+yuernestliu@users.noreply.github.com>
2022-09-15 13:48:27 +0800

    Delete Dijkstra directory

--- Dijkstra/DijkstraArithmetic.m
+++
@@ -1,8 +0,0 @@
-function [ Optimal, D] = DijkstraArithmetic(Table, dim, ORIGI, DESTI )
-
-[path, D]=Compute(Table, dim, ORIGI, DESTI); % Get all shortest paths (path) and all shortest cost (D) 
-
-cur=[];
-V=[];
-[Optimal]=print_path( ORIGI, DESTI, cur,V , path );
-
